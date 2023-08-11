2d9a2418227f7e89d8e69395461af24e1a2ea575
Adam Powis <a.powis.91@gmail.com>
2023-08-11 16:20:20 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # pyproject-bump-version
 A GitHub Action to bump pyproject.toml version numbers during pull requests
+
+This action will bump the version number of `pyproject.toml` file with respect to the main branch.
+
+For instance, say the version numbers in `branch1` and `main` are both `1.0.0` and a pull request is being made from `branch1` into `main`, then this action will bump the version number to `2.0.0` (if you specify `major` as the `bump_type` input).
+
+Note, that this action will only bump the version if the version number in main is less than or equal to the compare branch version.
+
+You can control whether you want the bump to happen before merging or after by checking the variable `github.event.pull_request.merged`.
+
+
+## Example Usage:
+
+The following example will do the following:
+1. Only runs on a pull request into main and when any files in the directory `python_project/` have changed or the `pyproject.toml` file has changed.
+2. The job only runs if the pull request is not being merged. This is useful for when a repository is set up to not allow writing directly to main.
+3. It will do a minor version bump to the `pyproject.toml` and commit the changes to the compare branch.
+
+```yaml
+name: "Version bumper"
+on:
+  pull_request:
+    branches:
+      - main
+    paths:
+      - 'python_project/**'
+
+jobs:
+  bump-version:
+    if: github.event.pull_request.merged == false
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          repository: ${{ github.event.pull_request.head.repo.full_name }}
+          ref: ${{ github.event.pull_request.head.ref }}
+          fetch-depth: "0"
+
+      - name: Version bumper
+        uses: "./.github/actions/version-bumper/"
+        with:
+          file_to_bump: "./pyproject.toml"
+          bump_type: "minor"
+          main_branch: "main"
+```
+
+
+# TODO List
+- Work out more about the fetch depth and potentially using different branches
+- Document `version_compare.py` more and add some tests.
+- Add more options to EndBug/add-and-commit@v9 inputs
+- Add option to not merge or not depending on pull request merge status
+- Implememt more file types than pyproject.toml?
+- Long term, think about using npm stuff instead of Python scripts. Probably will never get to doing this.
---
+++ version_compare.py
@@ -0,0 +1,102 @@
+import argparse
+from pathlib import Path
+from subprocess import check_output
+from typing import Literal, Optional, Tuple
+
+from packaging.version import parse, Version
+import toml
+
+
+def get_main_version(project_file: Path, main_branch: Optional[str] = None) -> Version:
+    """
+    Uses git cat-file to toml load the pyproject file and returns the verison number
+    """
+    main_branch = main_branch if main_branch else "main"
+    pyproject = check_output(["git", "cat-file", "blob", f"origin/main:./{project_file}"])
+    version_string = toml.loads(pyproject.decode())["project"]["version"]
+    return parse(version_string)
+
+
+def get_current_version(pyproject: str) -> Version:
+    """
+    Toml loads the pyproject file and returns the verison number.
+    """
+    with open(pyproject, "r") as fh:
+        pyproject_dict = toml.load(fh)
+        version_string = pyproject_dict["project"]["version"]
+        return parse(version_string)
+
+
+def get_next_version(
+    version_number: Version, bump_type: Literal["major", "minor", "micro"]
+) -> str:
+    """Bumps the provided version_number by one"""
+    major, minor, micro = version_number.release
+    if bump_type == "major":
+        return f"{major + 1}.0.0"
+    if bump_type == "minor":
+        return f"{major}.{minor + 1}.0"
+    return f"{major}.{minor}.{micro + 1}"
+
+
+def bump_pyproject(pyproject: Path, new_version_number: str) -> None:
+    """Bumps the version number in the pyproject file"""
+    with pyproject.open("r") as fh:
+        d = toml.load(pyproject)
+        d["project"]["version"] = new_version_number
+    with pyproject.open("w") as fh:
+        toml.dump(d, fh)
+
+
+def parse_args() -> Tuple[str, str, str, Path]:
+    """get argparse values"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--main", help="Main branch name", required=True)
+    parser.add_argument(
+        "--bump_type",
+        help="Bump type",
+        choices=["major", "minor", "micro"],
+        required=True,
+    )
+    parser.add_argument("--pyproject", help="Path to pyproject.toml", required=True)
+    parser.add_argument(
+        "--bump_commit_file",
+        help="Path to file containing git commit message",
+        required=True,
+    )
+    args = parser.parse_args()
+    pyproject = Path(args.pyproject)
+    if not Path(pyproject).is_file():
+        raise FileNotFoundError(f"{pyproject} does not exist")
+
+    return (
+        args.main,
+        args.bump_type,
+        args.bump_commit_file,
+        pyproject,
+    )
+
+
+def main():
+    main_branch, bump_type, bump_commit_file, pyproject = parse_args()
+
+    main_version = get_main_version(pyproject, main_branch)
+    current_version = get_current_version(pyproject)
+
+    print(f"{main_branch} branch version: {main_version}")
+    print(f"Current branch version: {current_version}")
+    if current_version <= main_version:
+        new_version = get_next_version(main_version, bump_type)
+        bump_pyproject(pyproject, new_version)
+        bump_message = (
+            f"Bumped version from {current_version} to {new_version}"
+        )
+        print(bump_message)
+        with open(bump_commit_file, "w") as fh:
+            fh.write(bump_message)
+    else:
+        print('No need to bump the version this time.')
+
+
+if __name__ == "__main__":
+    main()
